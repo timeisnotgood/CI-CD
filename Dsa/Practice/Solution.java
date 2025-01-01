88dc684667d45be0beb11bdd1b3d931edccde0c7
Bhoopathi Rs <97438769+timeisnotgood@users.noreply.github.com>
2025-01-01 07:10:53 +0000

    Binary tree practice

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,116 +1,82 @@
 package Practice;
 
-class Stak {
-    int top = -1;
-    int arr[];
-
-    Stak(int capacit){
-        this.arr = new int[capacit];
-    }
-
-    void Push(int val){
-        if (top == 10) {
-            System.out.println("stack is Bull");
-        }
-
-        top = top + 1;
-        arr[top] = val;
-    }
-
-    void Pop(){
-        if (top == -1) {
-            System.out.println("No data in Stack");
-            return;
-        }
-
-        top = top - 1;
-    }
-
-    void getDatas(){
-        for (int i = 0; i <= top; i++) {
-            System.out.println("---> " + arr[i]);
-        }
-    }
-    
-}
-
-class Queue {
-
-    int currentsiz = -1;
-    int start = -1;
-    int top = -1;
-    int capacity;
-    int arr[];
-
-    Queue(int capacity){
-        this.capacity = capacity;
-        arr = new int[capacity];
-    }
-
-    void push(int value){
-        if (currentsiz == capacity) {
-            System.out.println("Queue is Full");
-            return;
-        }else if (start == -1) {
-            start = top = 0;
-        }else{
-            top = (top +1) % capacity;
-        }
-
-        arr[top] = value;
-    }
-
-    void Pop(){
-        if (top == -1) {
-            System.out.println("Queue is empty");
-            return;
-        }
-
-        start = start+1;
-    }
-
-    int Top(){
-        if (top == -1) {
-            System.out.println("Queue is empty");
-        }
-
-        return arr[start];
-    }
-
-    void getallDatas(){
-        for (int i = 0; i <= top; i++) {
-            System.out.println("---> " + arr[i]);
-        }
-    }
-}
-
-
 public class Solution {
 
-    public static void main(String[] args) {
-        // Stak users = new Stak(10);
-        int ids[] = {4,7,2,8,2,23,74};
+    static class Tree {
+        int data;
+        Tree left, right;
 
-        // for (int i = 0; i < ids.length; i++) {
-        //     users.Push(ids[i]);
-        // }
+        Tree(int data){
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
 
-        // users.getDatas();
+    Tree root;
 
-        Queue qq = new Queue(10);
+    Solution(){
+        this.root = null;
+    }
 
-        for (int i = 0; i < ids.length; i++) {
-            qq.push(ids[i]);
+    void insert(int val){
+        root = insertRec(root, val);
+    }
+
+    Tree insertRec(Tree root, int val){
+        if (root == null) {
+            root = new Tree(val);
+            return root;
         }
 
-        qq.getallDatas();
-        System.out.println(
-            "dfdfg" + 
-            qq.Top()
+        if (val < root.data) {
+            root.left = insertRec(root.left, val);
+        }else if (val > root.data) {
+            root.right = insertRec(root.right, val);
+        }
 
-        );
-
-
+        return root;
     }
-    
+
+    void preOrderTraversal(Tree root){
+        if (root != null) {
+            System.out.println(root.data);
+            preOrderTraversal(root.left);
+            preOrderTraversal(root.right);
+        }
+    }
+
+    void inOrderTraversal(Tree root){
+        if (root != null) {
+            preOrderTraversal(root.left);
+            System.out.println(root.data);
+            preOrderTraversal(root.right);
+        }
+    }
+
+    Boolean search(Tree root, int val ){
+        if (root == null) {
+            return false;
+        }else if (root.data == val) {
+            return true;
+        }
+        
+        return val < root.data ? 
+        search(root.left, val) : search(root.right, val);
+    }
+
+    public static void main(String[] args) {
+
+        Solution bineTree = new Solution();
+
+        bineTree.insert(1);
+        bineTree.insert(2);
+        bineTree.insert(3);
+        bineTree.insert(4);
+
+        System.out.println("Traversal ---->");
+        bineTree.preOrderTraversal(bineTree.root);
+
+        System.out.println("Search ---> ");
+        System.out.println("Key " + 3 + "--> " + bineTree.search(bineTree.root, 2));
+    }
 }
