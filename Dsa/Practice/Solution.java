f2289605da44b7986cf1af383c5a7bd23ffc030d
Bhoopathi Rs <97438769+timeisnotgood@users.noreply.github.com>
2025-01-17 10:04:05 +0000

    prac

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,88 +1,60 @@
 package Practice;
 
-import java.util.LinkedList;
-
 public class Solution {
 
-    static class HashTable<K, V> {
-        class Entry<K, V> {
-            K key;
-            V value;
-            Entry(K key, V value){
-                this.key = key;
-                this.value = value;
-            }
+    static class Tree {
+        int data;
+        Tree left, right;
 
-            @Override
-            public String toString() {
-                return key + "=" + value;
-            }
-        }
-
-        LinkedList<Entry<K,V>>[] table;
-        int cap;
-
-        HashTable(int capacity){
-            this.cap = capacity;
-            this.table = new LinkedList[cap];
-
-            for (int i = 0; i < cap; i++) {
-                table[i] = new LinkedList<>();
-            }
-        }
-
-        int getHash(K key){
-            return Math.abs(key.hashCode()) % cap;
-        }
-
-        void push(K key, V value){
-            int index = getHash(key);
-            for (Entry<K,V> linkedList : table[index]) {
-                if (linkedList.key.equals(key)) {
-                    linkedList.value = value;
-                    return;
-                }
-            }
-            table[index].add(new Entry(key, value));
-        }
-
-        public V get(K key){
-            int index = getHash(key);
-            for (Entry<K,V> list : table[index]) {
-                if (list.key.equals(key)) {
-                    return list.value;
-                }
-            }
-            return null;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("{");
-            boolean first = true;
-            for (LinkedList<Entry<K, V>> bucket : table) {
-                for (Entry<K, V> entry : bucket) {
-                    if (!first) {
-                        sb.append(", ");
-                    }
-                    sb.append(entry.toString());
-                    first = false;
-                }
-            }
-            sb.append("}");
-            return sb.toString();
+        Tree(int data){
+            this.data = data;
         }
     }
+
+    static class BinaryTree {
+        Tree root;
+
+        void insert(int data){
+            root = insertRec(data, root);
+        }
+
+        Tree insertRec(int data, Tree root){
+            if (root == null) {
+                root = new Tree(data);
+                return root;
+            }
+
+            if (data < root.data) {
+                root.left = insertRec(data, root.left);
+            }else if (data > root.data) {
+                root.right = insertRec(data, root.right);
+            }
+            return root;
+        }
+
+        void PostOrderTraversal(Tree root){
+            if (root != null) {
+                PostOrderTraversal(root.left);
+                System.out.println("--> " + root.data);
+                PostOrderTraversal(root.right);
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        BinaryTree rt = new BinaryTree();
 
-        HashTable obj = new HashTable<>(10);
+        rt.insert(2);
+        rt.insert(3);
+        rt.insert(4);
+        rt.insert(5);
+        rt.insert(1);
 
-        obj.push(1, 1);
-        obj.push(2, 2);
-        obj.push(3, 3);
-        obj.push(4, 4);
 
-        System.out.println("--->" + obj.toString());
-    }   
+        rt.PostOrderTraversal(rt.root);
+    }
+
+
+
+    
 }
