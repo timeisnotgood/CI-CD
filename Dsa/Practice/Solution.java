7e665baa7f6badeadad821bbdb8555f41814d07f
Bhoopathi Rs <97438769+timeisnotgood@users.noreply.github.com>
2025-03-16 05:35:06 +0000

    queue practice

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,93 +1,54 @@
 package Practice;
 
-import java.security.KeyStore.Entry;
-import java.util.LinkedList;
+class Lnode{
+    int data;
+    Lnode next;
+    Lnode(int data){
+        this.data = data;
+    }
+}
 
-public class Solution <K, V> {
+class Queue {
+    Lnode start, end;
+    int size;
 
-    static class Entry<K, V> {
-        K key;
-        V val;
-
-        Entry(K key, V val){
-            this.key = key;
-            this.val = val;
+    void push(int val){
+        Lnode newVal = new Lnode(val);
+        if (start == null) {
+            start = end = newVal;
         }
 
-        @Override
-        public String toString() {
-            return "(" + key + "--" + val + ")";
+        end.next = newVal;
+        end = newVal;
+    }
+
+    void getAll(){
+        Lnode temp = start;
+
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
         }
     }
 
-    LinkedList<Entry<K, V>>[] bucket;
-    int capacity;
-
-    Solution(int cap){
-        this.capacity = cap;
-        this.bucket = new LinkedList[capacity];
-
-        for (int i = 0; i < bucket.length; i++) {
-            this.bucket[i] = new LinkedList<>();
-        }
+    int getTop(){
+        return end.data;
     }
+}
 
-    int getindex(K key){
-        return Math.abs(key.hashCode()) % capacity;
-    }
-
-    void put(K key, V val){
-        int index = getindex(key);
-
-        for(Entry<K, V> list : bucket[index]){
-            if (list.key == key) {
-                list.val = val;
-                return;
-            }
-        }
-
-        bucket[index].add(new Entry<K,V>(key, val));
-    }
-
-    V getValue(K key){
-        int index = getindex(key);
-
-        for(Entry<K, V> list : bucket[index]){
-            if (list.key.equals(key)) {
-                System.out.println(list.val);
-            }
-        }
-
-        return null;
-    }
-
-    void printAll(){
-        for (int i = 0; i < capacity; i++) {
-            System.out.println("Bucket ---> " + i);
-            if (bucket[i].isEmpty()) {
-                System.out.println("Table is Empty !");
-            }else{
-                for(Entry<K, V> list : bucket[i]){
-                    System.out.println(list);
-                }
-                System.out.println();
-            }
-        }
-    }
+public class Solution {
 
     public static void main(String[] args) {
-        Solution table = new Solution<>(5);
+        Queue q = new Queue();
 
-        table.put("one", "lissan");
-        table.put("two", "kishore");
-        table.put("three", "akshay");
-        table.put("four", "gokul");
+        q.push(1);
+        q.push(2);
+        q.push(3);
+        q.push(4);
 
-        table.getValue("three");
+        q.getAll();
 
-        System.out.println("-------------->");
-
-        table.printAll();
-        
+        System.out.println("------->");
+        System.out.println(q.getTop());
     }
 }
