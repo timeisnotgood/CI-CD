80dec48e3136c9b19e0290547a5816c4bbc0edd0
Bhoopathi Rs <97438769+timeisnotgood@users.noreply.github.com>
2025-03-16 05:30:05 +0000

    hash table prac

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,78 +1,93 @@
 package Practice;
 
-class Linkedl{
-    int data;
-    Linkedl next;
-    Linkedl(int data){
-        this.data = data;
-        this.next = null;
-    }
-}
+import java.security.KeyStore.Entry;
+import java.util.LinkedList;
 
-class stack {
-    Linkedl top;
+public class Solution <K, V> {
 
-    void push(int data){
-        Linkedl newval = new Linkedl(data);
-       newval.next = top;
-       top = newval;
-    }
+    static class Entry<K, V> {
+        K key;
+        V val;
 
-    void startQueue(){
-        Linkedl temp = top;
-
-        while (temp != null) {
-            System.out.println("---->" + temp.data);
-            temp = temp.next;
-        }
-    }
-}
-class Queue{
-    Linkedl start, end;
-
-    void push(int val){
-        Linkedl newVal = new Linkedl(val);
-        if (start == null) {
-            start = end = newVal;
+        Entry(K key, V val){
+            this.key = key;
+            this.val = val;
         }
 
-        end.next = newVal;
-        end = newVal;
-    }
-
-    void startQueue(){
-        Linkedl temp = start;
-
-        while (temp != null) {
-            System.out.println("---->" + temp.data);
-            temp = temp.next;
+        @Override
+        public String toString() {
+            return "(" + key + "--" + val + ")";
         }
     }
 
-    void endQueue(){
-        Linkedl temp = end;
+    LinkedList<Entry<K, V>>[] bucket;
+    int capacity;
 
-        while (temp != null) {
-            System.out.println("---->" + temp.data);
-            temp = temp.next;
+    Solution(int cap){
+        this.capacity = cap;
+        this.bucket = new LinkedList[capacity];
+
+        for (int i = 0; i < bucket.length; i++) {
+            this.bucket[i] = new LinkedList<>();
         }
     }
 
-}
+    int getindex(K key){
+        return Math.abs(key.hashCode()) % capacity;
+    }
 
-public class Solution {
+    void put(K key, V val){
+        int index = getindex(key);
+
+        for(Entry<K, V> list : bucket[index]){
+            if (list.key == key) {
+                list.val = val;
+                return;
+            }
+        }
+
+        bucket[index].add(new Entry<K,V>(key, val));
+    }
+
+    V getValue(K key){
+        int index = getindex(key);
+
+        for(Entry<K, V> list : bucket[index]){
+            if (list.key.equals(key)) {
+                System.out.println(list.val);
+            }
+        }
+
+        return null;
+    }
+
+    void printAll(){
+        for (int i = 0; i < capacity; i++) {
+            System.out.println("Bucket ---> " + i);
+            if (bucket[i].isEmpty()) {
+                System.out.println("Table is Empty !");
+            }else{
+                for(Entry<K, V> list : bucket[i]){
+                    System.out.println(list);
+                }
+                System.out.println();
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        stack ss = new stack();
+        Solution table = new Solution<>(5);
 
-        ss.push(1);
-        ss.push(2);
-        ss.push(3);
-        ss.push(4);
-        ss.push(5);
+        table.put("one", "lissan");
+        table.put("two", "kishore");
+        table.put("three", "akshay");
+        table.put("four", "gokul");
 
-        ss.startQueue();
+        table.getValue("three");
+
+        System.out.println("-------------->");
+
+        table.printAll();
+        
     }
-
-    
 }
