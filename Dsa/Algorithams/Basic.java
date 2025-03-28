6603a147c1dfbf7cd421c5f79e85cc69bd448d28
bhoopathi <bhoopathicl@gmail.com>
2025-03-28 07:17:45 +0530

    Add isSorted method to check if an array is sorted with at most one rotation

--- Dsa/Algorithams/Basic.java
+++ Dsa/Algorithams/Basic.java
@@ -173,6 +173,13 @@ public class Basic {
         return j;
     }
 
+    public boolean isSorted(int[] nums) {
+        int count = 0, n = nums.length;
+         for (int i = 0; i < n; i++) 
+             if (nums[i] > nums[(i+1) % n] && ++count > 1) return false;
+         return count <= 1;
+     }
+
     public static void main(String[] args) {
 
        Basic algo = new Basic();
