4cf6ea562abd561b9f81c91d436f93e5bc2477b5
bhoopathi <bhoopathicl@gmail.com>
2025-03-29 16:32:45 +0530

    two pointer

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -2,77 +2,72 @@ package Practice;
 
 public class Solution {
 
-    int[] heap;
-    int cap, size;
-
-    Solution(int cap){
-        this.cap = cap;
-        this.heap = new int[this.cap];
-        this.size = 0;
-    }
-
-    int getparentIndex(int i) {return (i-1)/2;};
-    int getLeftIndex(int i) {return (i*2)+1;};
-    int getRightIndex(int i) {return (i*2)+2;};
-
-    Boolean hasParent(int i) {return getparentIndex(i) >= 0;};
-    Boolean hasLeft(int i) {return getLeftIndex(i) < size;};
-    Boolean hasRight(int i) {return getRightIndex(i) < size;};
-
-    int Parent(int i) {return heap[getparentIndex(i)];};
-    int Left(int i) {return heap[getLeftIndex(i)];};
-    int right(int i) {return heap[getRightIndex(i)];};
-
-    void insert(int val){
-        if (size + 1 == cap) {
-            throw new IllegalStateException("Heap is bull");
-        }
-
-        heap[size] = val;
-        size++;
-        heapifyUp();
-    }
-
-    void heapifyUp(){
-        int index = size - 1;
-
-        while (hasParent(index) && Parent(index) > heap[index]) {
-            swap(getparentIndex(index), index);
-            index = getparentIndex(index);
-        }
-    }
-
-    void swap(int i, int j){
-        int temp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = temp;
-    }
-
-    int extractMin(){
-        if (size  == -1) {
-            throw new IllegalStateException("No element");
-        }
-
-        int deleted = heap[0];
-        heap[0] = heap[size - 1];
-        size--;
-        heapifyDown();
-        return deleted;
-    }
-
-    void heapifyDown(){
-        int index = 0;
-
-        while (hasLeft(index)) {
-            int smallest = getLeftIndex(index);
-            if (hasRight(index) && right(index) < Left(index)) {
-                smallest = getRightIndex(index);
+    void selectionSort(int arr[], int n){
+        for(int i = 0; i <= n-1; i++){
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
             }
 
-            if (heap[index] < heap[smallest]) {break;}
-            else{swap(index, smallest);}
-            index = smallest;
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
+    }
+
+    void bubbleSort( int arr[], int n){
+        for(int i = n-1; i >= 0; i--){
+            int swap = 0;
+            for(int j = 0; j <= i -1; j++){
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                    swap = 1;
+                }
+            }
+
+            if (swap == 0) break;
+        }
+    }
+
+    void insertionSort(int arr[], int n){
+        for(int i = 0; i <= n - 1; i++){
+            int j = i;
+            while (j > 0 && arr[j-1] > arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+        }
+    }
+
+    int deleteDuplicate(int[] arr){
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+
+
+    public static void main(String[] args) {
+
+        Solution algo = new Solution();
+
+        int[] arr = {13,46,24,52,20,9};
+        // System.out.println("Before ---> " + arr.toString());
+        // algo.insertionSort(arr, arr.length );
+
+        int[] arr2 = {0,0,1,1,1,2,2,3,3,4};
+        algo.deleteDuplicate(arr2);
+        
     }
 
     
