dcd6e0311d4a152548ce236084dcbf72e5729004
bhoopathi <bhoopathicl@gmail.com>
2025-04-01 21:05:04 +0530

    longest and dupllicate element prac

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -2,97 +2,27 @@ package Practice;
 
 public class Solution {
 
-    void selectionSort(int arr[], int n){
-        for(int i = 0; i <= n-1; i++){
-            int min = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
-                }
-            }
+    public static void main(String[] args) {
+        
+        int arr[] = {9,8,5,3,2,1,9,5};
 
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
-        }
-    }
+        int max = arr[0];
+        int count = 0;
 
-    void bubbleSort( int arr[], int n){
-        for(int i = n-1; i >= 0; i--){
-            int swap = 0;
-            for(int j = 0; j <= i -1; j++){
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = temp;
-                    swap = 1;
-                }
-            }
 
-            if (swap == 0) break;
-        }
-    }
-
-    void insertionSort(int arr[], int n){
-        for(int i = 0; i <= n - 1; i++){
-            int j = i;
-            while (j > 0 && arr[j-1] > arr[j]) {
-                int temp = arr[j];
-                arr[j] = arr[j-1];
-                arr[j-1] = temp;
-                j--;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
-    }
-
-    int deleteDuplicate(int[] arr){
-        int i = 0;
-        for (int j = 1; j < arr.length; j++) {
-            if (arr[i] != arr[j]) {
-                i++;
-                arr[i] = arr[j];
-            }
-        }
-        return i + 1;
-    }
-
-    void reverse(){
-        int[]arr = {-1,-100,3,99};
-        int temp = arr[arr.length - 1];
-        int k = 3;
-
-        for(int j = 0; j < k - 1; j++){
-            for (int i = arr.length - 1; i >= 0; i--) {
-                if (i == 0) {
-                    arr[i] = temp;
-                    temp = arr[arr.length - 1];
-                    break;
-                }
-                arr[i] = arr[i-1];
-            }
-        }
-
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(" " + arr[i]);
+            if (arr[i] == max) {
+                count ++;
+            }
         }
 
-        System.out.println("------->" + k);
+        System.out.println("dsgfdfg---" + max +  " " + count);
     }
-
-
-    public static void main(String[] args) {
-
-        Solution algo = new Solution();
-
-        int[] arr = {13,46,24,52,20,9};
-        // System.out.println("Before ---> " + arr.toString());
-        // algo.insertionSort(arr, arr.length );
-
-        // int[] arr2 = {0,0,1,1,1,2,2,3,3,4};
-        algo.reverse();
-        
-    }
-
     
 }
