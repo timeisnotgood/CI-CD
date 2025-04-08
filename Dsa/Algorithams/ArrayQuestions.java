854341263f128e4ef5be784198b86e950df95af8
bhoopathi <bhoopathicl@gmail.com>
2025-04-08 11:44:51 +0530

    Binary search practice

--- Dsa/Algorithams/ArrayQuestions.java
+++ Dsa/Algorithams/ArrayQuestions.java
@@ -245,6 +245,32 @@ public class ArrayQuestions {
     }
 
 
+    public int binarySearch(){
+        int arr[] = {2, 4, 10, 15, 18, 21, 36};
+        int target = 15;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) /2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+
 
 
 
@@ -252,9 +278,9 @@ public class ArrayQuestions {
         ArrayQuestions questions = new ArrayQuestions();
 
         int[] arr = {4,1,2,1,2};
-        // System.out.println("dfgdfsg---->" +
-        questions.maxSubArray();
-        // );
+        System.out.println("dfgdfsg---->" +
+        questions.binarySearch()
+        ); 
 
     }
 }
--- Dsa/Algorithams/Basic.java
+++ Dsa/Algorithams/Basic.java
@@ -180,6 +180,32 @@ public class Basic {
          return count <= 1;
      }
 
+    public int binarySearch(){
+        int arr[] = {2, 4, 10, 15, 18, 21, 36};
+        int target = 15;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int mid = left - (right - left) /2;
+
+            if (arr[mid] == target) {
+                return arr[mid];
+            }
+
+            if (arr[mid] > target) {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+            
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
 
        Basic algo = new Basic();
