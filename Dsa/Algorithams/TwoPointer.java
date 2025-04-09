ac7648f8bada83a03ab9b5090d5b676aa0c18491
bhoopathi <bhoopathicl@gmail.com>
2025-04-09 21:11:33 +0530

    practice

--- Dsa/Algorithams/TwoPointer.java
+++ Dsa/Algorithams/TwoPointer.java
@@ -1,5 +1,7 @@
 package Algorithams;
 
+import java.util.ArrayList;
+
 public class TwoPointer {
 
     public void subArray(){
@@ -23,8 +25,42 @@ public class TwoPointer {
         System.out.println("-----> " + max);
     }
 
+    public void findNegative(){
+        int arr[] = {12, -1, -7, 8, -15, 30, 16, 28};
+        int k = 3;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int i = 0;
+        int j = k;
+
+        while (i < j) {
+            System.out.println(" -- " + i);
+            i++;
+        }
+
+    }
+
+    public int[] Twopointer(){
+        int arr[] = {1, 2, 3, 4, 6};
+        int k = 6;
+
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == k) return new int[]{arr[left] , arr[right]};
+            if (sum < k) left++;
+            else right--;
+
+        }
+
+        return new int[0];
+
+    }
+
     public static void main(String[] args) {
        TwoPointer pointer = new TwoPointer();
-       pointer.subArray();
+       pointer.Twopointer();
     }
 }
