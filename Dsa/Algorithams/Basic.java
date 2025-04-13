0dee556d3e5ae9cd531e55de5ddb123bebc155ef
bhoopathi <bhoopathicl@gmail.com>
2025-04-13 18:42:06 +0530

    fixes

--- Dsa/Algorithams/Basic.java
+++ Dsa/Algorithams/Basic.java
@@ -39,10 +39,10 @@ public class Basic {
     }
 
     void BubbleSort(int[] arr, int n){
-        for(int i = n -1; i >= 0; i++){
+        for(int i = n -1; i >= 0; i--){
             int swap = 0;
 
-            for(int j = 0; j < i -1;j++){
+            for(int j = 0; j <= i -1;j++){
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
