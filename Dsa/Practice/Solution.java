0de0e59160ea917f96222cdd513554c9ede056ba
bhoopathi <bhoopathicl@gmail.com>
2025-05-05 07:12:02 +0530

    Quick sort

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -45,12 +45,47 @@ public class Solution {
         }
     }
 
+    void quickSort(int[] arr, int low, int high){
+        if (low < high) {
+            int peviotIndex = partetion(arr, low, high);
+            quickSort(arr, low, peviotIndex);
+            quickSort(arr, peviotIndex + 1, high);
+        }
+    }
+
+    int partetion(int[] arr, int low, int high){
+        int peviot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (arr[i] <= peviot && i <= high - 1) {
+                i++;
+            }
+
+            while (arr[j] > peviot && j >= low + 1) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        return j;
+    }
+
     public static void main(String[] args) {
         Solution obj = new Solution();
 
         int[] arr = {38, 27, 43, 3, 9, 82, 10};
 
-        obj.mergeSort(arr, 0, arr.length - 1);
+        obj.quickSort(arr, 0, arr.length - 1);
 
         for(int a : arr){
             System.out.print(a + " ");
