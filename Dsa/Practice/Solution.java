a9c66ccc70405fc8f6bdc2dc826dfb613686395f
bhoopathi <bhoopathicl@gmail.com>
2025-05-05 06:37:33 +0530

    merge Sort

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,20 +1,59 @@
 package Practice;
 
+import java.util.ArrayList;
+
 public class Solution {
 
-    int heap[];
-    int size, cap;
+    void mergeSort(int[] arr, int low, int high){
+        if (low < high) {
+            int mid = (low + high) / 2;
 
-    Solution(int cap){
-        this.cap = cap;
-        this.size = 0;
-        this.heap = new int[cap];
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
     }
 
-    int parentIndex(int i){return (i-1) / 2;}
-    int leftIndex(int i){return (i * 2) + 1;}
+    void merge(int[] arr, int low, int mid, int high){
+        ArrayList<Integer> list = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+
+        while (left <= mid && right <= high) {
+            if (arr[left] < arr[right]) {
+                list.add(arr[left]);
+                left++;
+            }else{
+                list.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            list.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            list.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = list.get(i - low);
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("one");
+        Solution obj = new Solution();
+
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+
+        obj.mergeSort(arr, 0, arr.length - 1);
+
+        for(int a : arr){
+            System.out.print(a + " ");
+        }
     }
 }
