af619aab2192af7fa82a8801a162378edc4ce6be
bhoopathi <bhoopathicl@gmail.com>
2025-05-06 07:20:06 +0530

    min Heap implemented

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,114 +1,83 @@
 package Practice;
 
-import java.util.ArrayList;
-
 public class Solution {
 
-    void mergeSort(int arr[], int low, int high){
-        if(low < high){
-            int mid = (low + high) /2;
+    int[] heap;
+    int size, cap;
 
-            mergeSort(arr, low, mid);
-            mergeSort(arr, mid+1, high);
-            merge(arr, low, mid, high);
+    Solution(int cap){
+        this.cap = cap;
+        this.size = 0;
+        this.heap = new int[cap];
+    }
+
+    int parentIndex(int i){return (i - 1) /2;}
+    int leftIndex(int i){return (i*2) + 1;}
+    int rightIndex(int i){return (i*2) + 2;}
+
+    Boolean hasParent(int i){return parentIndex(i) >= 0;}
+    Boolean hasLeft(int i){return leftIndex(i) < size;}
+    Boolean hasRight(int i){return rightIndex(i) < size;}
+
+    int parent(int i){return heap[parentIndex(i)];}
+    int left(int i){return heap[leftIndex(i)];}
+    int right(int i){return heap[rightIndex(i)];}
+
+    void swap(int i, int j){
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    void heapfyUp(){
+        int index = size - 1;
+
+        while (hasParent(index) && parent(index) > heap[index]) {
+            swap(parentIndex(index), index);
+            index = parentIndex(index);
         }
     }
 
-    void merge(int arr[], int low, int mid, int high){
-        ArrayList<Integer> list = new ArrayList<>();
-        int left = 0;
-        int right = mid + 1;
-        
-        while (left <= mid && right <= high) {
-            if (arr[left] < arr[right]) {
-                list.add(arr[left]);
-                left++;
+    void heapfyDown(){
+        int index = 0;
+
+        while (hasLeft(index)) {
+            int smallestValue = leftIndex(index);
+
+            if (hasRight(index) && right(index) < left(index)) {
+                smallestValue = rightIndex(index);
+            }
+
+            if (heap[index] <= heap[smallestValue]) {
+                break;
             }else{
-                list.add(arr[right]);
-                right++;
+                swap(index, smallestValue);
             }
-        }
-
-        while (left <= mid) {
-            list.add(arr[left]);
-            left++;
-        }
-
-        while (right <= high) {
-            list.add(arr[right]);
-            right++;
-        }
-
-        for (int i = low; i <= high; i++) {
-            arr[i] = list.get(i - low);
+            index = smallestValue;
         }
     }
 
-    void quickSort(int[] arr, int low, int high){
-        if (low < high) {
-            int peviotIndex = perception(arr, low, high);
-            quickSort(arr, low, peviotIndex - 1);
-            quickSort(arr, peviotIndex + 1, high);
-        }
-    }
-
-    int perception(int[] arr, int low, int high){
-        int peviot = arr[low];
-        int i = low;
-        int j = high;
-
-        while (i < j) {
-            while (arr[i] <= peviot && i <= high - 1) {
-                i++;
-            }
-
-            while (arr[j] > peviot && j >= low + 1) {
-                j--;
-            }
-
-            if (i < j) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    void insert(int val){
+        if (size == cap) {
+            System.out.println("Heap is Full !! ");
+            return;
         }
 
-        int temp = arr[low];
-        arr[low] = arr[j];
-        arr[j] = temp;
-
-        return j;
+        heap[size] = val;
+        size++;
+        heapfyUp();
     }
 
-    int binarySearch(int[] arr, int val){
-
-        int low = 0;
-        int high = arr.length - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (arr[mid] == val) {
-                return mid;
-            }
-            
-            if(arr[mid] < val){
-                low = mid + 1;
-            }else{
-                high = mid - 1;
-            }
+    int extractMin(){
+        if (size == 0) {
+            System.out.println("Heap is empty !!");
+            return -1;
         }
-        return -1;
-    }
 
-    public static void main(String[] args) {
-
-        Solution obj = new Solution();
-        int arr[] = {38, 27, 43, 3, 9, 82, 10};
-
-        System.out.print( "Index --> " +
-            obj.binarySearch(arr, 9)
-        );
-
+        int deleted = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        heapfyDown();
+        return deleted;
     }
 }
