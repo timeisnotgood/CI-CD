ae091cbdbd81113c554af79285495132933c5d23
bhoopathi <bhoopathicl@gmail.com>
2025-05-07 06:57:29 +0530

    prac

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,120 +1,127 @@
 package Practice;
 
-import java.util.PriorityQueue;
-
 public class Solution {
 
     int[] heap;
-    int size, cap;
+    int size, capacity;
 
-    Solution(int cap){
-        this.cap = cap;
+    // Constructor
+    public Solution(int capacity) {
+        this.capacity = capacity;
         this.size = 0;
-        this.heap = new int[cap];
+        this.heap = new int[capacity];
     }
 
-    int parentIndex(int i){return (i - 1) /2;}
-    int leftIndex(int i){return (i*2) + 1;}
-    int rightIndex(int i){return (i*2) + 2;}
+    // Helper methods for index calculation
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return 2 * i + 1; }
+    int right(int i) { return 2 * i + 2; }
 
-    Boolean hasParent(int i){return parentIndex(i) >= 0;}
-    Boolean hasLeft(int i){return leftIndex(i) < size;}
-    Boolean hasRight(int i){return rightIndex(i) < size;}
-
-    int parent(int i){return heap[parentIndex(i)];}
-    int left(int i){return heap[leftIndex(i)];}
-    int right(int i){return heap[rightIndex(i)];}
-
-    void swap(int i, int j){
+    // Swap elements
+    void swap(int i, int j) {
         int temp = heap[i];
         heap[i] = heap[j];
         heap[j] = temp;
     }
 
-    void heapfyUp(){
+    // Heapify up after insert
+    void heapifyUp() {
         int index = size - 1;
-
-        while (hasParent(index) && parent(index) > heap[index]) {
-            swap(parentIndex(index), index);
-            index = parentIndex(index);
+        while (index > 0 && heap[parent(index)] > heap[index]) {
+            swap(index, parent(index));
+            index = parent(index);
         }
     }
 
-    void heapfyDown(){
+    // Heapify down after remove
+    void heapifyDown() {
         int index = 0;
 
-        while (hasLeft(index)) {
-            int smallestValue = leftIndex(index);
+        while (left(index) < size) {
+            int smallerChild = left(index);
 
-            if (hasRight(index) && right(index) < left(index)) {
-                smallestValue = rightIndex(index);
+            if (right(index) < size && heap[right(index)] < heap[left(index)]) {
+                smallerChild = right(index);
             }
 
-            if (heap[index] <= heap[smallestValue]) {
-                break;
-            }else{
-                swap(index, smallestValue);
-            }
-            index = smallestValue;
+            if (heap[index] <= heap[smallerChild]) break;
+
+            swap(index, smallerChild);
+            index = smallerChild;
         }
     }
 
-    void insert(int val){
-        if (size == cap) {
-            System.out.println("Heap is Full !! ");
+    // Insert a new value into the heap
+    public void insert(int value) {
+        if (size == capacity) {
+            System.out.println("Heap is full!");
             return;
         }
 
-        heap[size] = val;
+        heap[size] = value;
         size++;
-        heapfyUp();
+        heapifyUp();
     }
 
-    int extractMin(){
+    // Remove and return the minimum value (root)
+    public int poll() {
         if (size == 0) {
-            System.out.println("Heap is empty !!");
+            System.out.println("Heap is empty!");
             return -1;
         }
 
-        int deleted = heap[0];
+        int min = heap[0];
         heap[0] = heap[size - 1];
         size--;
-        heapfyDown();
-        return deleted;
+        heapifyDown();
+
+        return min;
     }
 
-     public int[] Klargest(int[] arr, int k){
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        
-        for(int num : arr){
-            minHeap.offer(num);
-            
-            if(minHeap.size() > k){
-                minHeap.poll();
-            }
+    // Peek the minimum value without removing it
+    public int peek() {
+        if (size == 0) {
+            System.out.println("Heap is empty!");
+            return -1;
         }
-        
-        int[] result = new int[k];
-       int i = 0;
-       
-       for(int num : minHeap){
-           result[i] = num;
-           i++;
-       }
-       
-       return result;
+        return heap[0];
     }
+
+    // Check if the heap is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Print the heap
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i] + " ");
+        }
+        System.out.println();
+    }
+
 
     public static void main(String[] args) {
-        Solution obj = new Solution(10);
 
-        int[] arr = {0, 5, 20, 100, 70, 30};
-        int k = 3;
-        int[] largest = obj.Klargest(arr, k);
+        Solution pq = new Solution(10);
+
+       int[] arr = {10, 5, 20, 100, 70, 30};
+
+       int k = 3;
+
+
+       for(int num : arr){
+            pq.insert(num);
+            if (pq.size > k) {
+                pq.poll();
+            }
+       }
+
+       int[] result = new int[k];
+       int i = 0;
+       for(int num : pq.heap){
+        result[i] = num;
+       }
         
-        System.out.print("Heap element :--> ");
-        for(int num : largest){
-            System.out.print(" " + num);
-        }
     }
 }
