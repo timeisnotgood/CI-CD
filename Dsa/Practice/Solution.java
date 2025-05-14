893df7b741fa51ca36fa6f05a11d3b07b2f3f899
bhoopathi <bhoopathicl@gmail.com>
2025-05-14 08:58:57 +0530

    matrix prac

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -42,7 +42,40 @@ public class Solution {
         }
 
         return matrix;
-     }
+    }
+
+    public void setZeroes(int[][] matrix) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean row[] = new boolean[m];
+        boolean col[] = new boolean[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                   row[i] = true;
+                   col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (row[i] == true) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (col[i] == true) {
+                for (int j = 0; j < m; j++) {
+                    matrix[j][j] = 0;
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> Matrix = new ArrayList<>();
