2f003c3c1de754cbc1f6d0d638c51ac31cc2a285
bhoopathi <bhoopathicl@gmail.com>
2025-05-14 18:32:15 +0530

    Bfs practice

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,102 +1,76 @@
 package Practice;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
 
-    static ArrayList<ArrayList<Integer>> matrixRow(ArrayList<ArrayList<Integer>> matrix, int m, int n, int i){
-        for (int j = 0; j < n; j++) {
-            if (matrix.get(i).get(j) != 0) {
-                matrix.get(i).set(j, -1);
-            }
+    ArrayList<ArrayList<Integer>> list;
+    int cap;
+
+    Solution(int cap){
+        this.cap = cap;
+        this.list = new ArrayList<>(cap);
+
+        for (int i = 0; i < cap; i++) {
+            list.add(new ArrayList<>());
         }
-        return matrix;
     }
 
-    static ArrayList<ArrayList<Integer>> matrixCol(ArrayList<ArrayList<Integer>> matrix, int m, int n, int i){
-        for (int j = 0; j < m; j++) {
-            if (matrix.get(j).get(i) != 0) {
-                matrix.get(j).set(i, -1);
-            }
-        }
-        return matrix;
+    void addNode(int v, int e){
+        list.get(v).add(e);
+        list.get(e).add(v);
     }
 
-    static ArrayList<ArrayList<Integer>> zeroMatrix( ArrayList<ArrayList<Integer>> matrix, int m, int n){
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix.get(i).get(j) == 0) {
-                    matrixRow(matrix, m, n, i);
-                    matrixCol(matrix, m, n, j);
-                }
+    void printAdj(ArrayList<ArrayList<Integer>> list){
+        int i = 0;
+        for(ArrayList<Integer> node : list ){
+            System.out.print(i + " ->  ");
+            for(Integer num : node){
+               System.out.print(num + " ");
             }
+            System.out.println();
+            i += 1;
         }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix.get(i).get(j) == -1) {
-                    matrix.get(i).set(j, 0);
-                }
-            }
-        }
-
-        return matrix;
     }
 
-    public void setZeroes(int[][] matrix) {
-        
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean row[] = new boolean[m];
-        boolean col[] = new boolean[n];
+    public ArrayList<Integer> bfsSearch(){
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean[] vis = new boolean[cap];
+        Queue<Integer> q = new LinkedList<>();
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                   row[i] = true;
-                   col[j] = true;
+        q.add(0);
+        vis[0] = true;
+
+        while (!q.isEmpty()) {
+            Integer node = q.poll();
+            bfs.add(node);
+
+            for(Integer it : list.get(node)){
+                if (vis[it] == false) {
+                    vis[it] = true;
+                    q.add(it);
                 }
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            if (row[i] == true) {
-                for (int j = 0; j < n; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (col[i] == true) {
-                for (int j = 0; j < m; j++) {
-                    matrix[j][j] = 0;
-                }
-            }
-        }
+        return bfs;
     }
 
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> Matrix = new ArrayList<>();
+        Solution obj = new Solution(7);
 
-        Matrix.add(new ArrayList<>(Arrays.asList(1,1,1)));
-        Matrix.add(new ArrayList<>(Arrays.asList(1,0,1)));
-        Matrix.add(new ArrayList<>(Arrays.asList(1,1,1)));
+        obj.addNode(0, 1);
+        obj.addNode(0, 2);
+        obj.addNode(1, 3);
+        obj.addNode(1, 4);
+        obj.addNode(2, 5);
+        obj.addNode(2, 6);
 
-        int m = Matrix.size();
-        int n = Matrix.get(0).size();
-
-        ArrayList<ArrayList<Integer>> ans = zeroMatrix(Matrix, m, n);
-
-        System.out.println("The Final matrix is: ");
-        System.out.println(" ");
-
-        for (ArrayList<Integer> row : ans) {
-            for (Integer ele : row) {
-                System.out.print(ele + " ");
-            }
-            System.out.println();
+        ArrayList<Integer> list = obj.bfsSearch();
+        for(Integer num : list){
+            System.out.println(" -- > " + num);
         }
     }
 }
