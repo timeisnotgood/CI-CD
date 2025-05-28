a423a53ec54d8d306ed4bf3cd3bd18502026ef4b
bhoopathi <bhoopathicl@gmail.com>
2025-05-28 06:54:44 +0530

    hashTable reviced

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,75 +1,43 @@
 package Practice;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 
-public class Solution {
+public class Solution<k, v> {
 
-    void mergeSort(int[] arr, int low, int high){
-        if (low < high) {
-            int mid = (low + high) /2;
-            mergeSort(arr, low, mid);
-            mergeSort(arr, mid + 1, high);
-            merge(arr, low, mid, high);
+    static class Entry<k,v> {
+        k key;
+        v val;
+        Entry(k key, v val){
+            this.key = key;
+            this.val = val;
         }
     }
 
-    void merge(int[] arr, int low, int mid, int high){
-        ArrayList<Integer> list = new ArrayList<>();
-        int left = low;
-        int right = mid + 1;
+    LinkedList<Entry<k,v>>[] bucket;
+    int cap;
 
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                list.add(arr[left]);
-                left++;
-            }else{
-                list.add(arr[right]);
-                right++;
+    Solution(int cap){
+        this.cap = cap;
+        this.bucket = new LinkedList[cap];
+        for (int i = 0; i < bucket.length; i++) {
+            this.bucket[i] = new LinkedList<>();
+        }
+    }
+
+    int index(k key){
+        return Math.abs(key.hashCode()) % this.cap;
+    }
+
+    void addPairs(k key, v val){
+        int i = index(key);
+
+        for(Entry<k, v> entry : bucket[i]){
+            if (entry.key.equals(key)) {
+                entry.val = val;
+                return;
             }
         }
+        bucket[i].add(new Entry(key, val));
     }
-
-    ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj, int cap){
-        ArrayList<Integer> bfs = new ArrayList<>();
-        boolean[] vis = new boolean[cap - 1];
-        Queue<Integer> q = new LinkedList<>();
-
-        vis[0] = true;
-        q.add(0);
-
-        while (!q.isEmpty()) {
-            Integer node = q.poll();
-            bfs.add(node);
-            for(int it : adj.get(node)){
-                if (vis[it] != true) {
-                    vis[it] = true;
-                    q.add(it);
-                }
-            }
-        }
-        return bfs;
-    } 
-
-    ArrayList<Integer> dfsmethod(ArrayList<ArrayList<Integer>> adj, int cap){
-        ArrayList<Integer> dfs = new ArrayList<>();
-        boolean[] vis = new boolean[cap - 1];
-        dfs(0, adj, dfs, vis);
-        return dfs;
-    }
-
-    void dfs(int node, ArrayList<ArrayList<Integer>> adj,  ArrayList<Integer> dfs, boolean[] vis){
-        vis[node] = true;
-        dfs.add(node);
-        for(int it : adj.get(node)){
-            if (vis[it] != true) {
-                dfs(it, adj, dfs, vis);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution obj = new Solution();
-    }
+    
 }
