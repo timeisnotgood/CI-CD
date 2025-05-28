63be7d4641137505e2ad62bbf8d03a48d4c16320
bhoopathi <bhoopathicl@gmail.com>
2025-05-28 07:32:10 +0530

    Binary tree reviced

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,130 +1,46 @@
 package Practice;
-/**
- * LinkedList
- */
-class LinkedLst {
-    int data;
-    LinkedLst next;
 
-    LinkedLst(int dat){
-        this.data = dat;
-        this.next = null;
-    }
+    class Node {
+        int data;
+        Node left, right;
 
-    LinkedLst(int dat, LinkedLst nxt){
-        this.data = dat;
-        this.next = nxt;
-    }
-}
-
-
-class stack {
-    int[] stk;
-    int cap, top;
-
-    stack(int cap){
-        this.cap = cap;
-        this.top = 0;
-        this.stk = new int[cap];
-    }
-
-    void push(int val){
-        if (top + 1 == cap) {
-            System.out.println("Stack is full");
-            return;
-        }
-
-        stk[top++] = val;
-    }
-
-    void pop(){
-        if (top == 0) {
-            System.out.println("Stk is empty");
-            return;
-        }
-        top--;
-    }
-    
-}
-
-class queue {
-    int[] que;
-    int top, end, cap, size;
-
-    queue(int cap){
-        this.cap = cap;
-        this.top = this.end = -1;
-        this.size = 0;
-        this.que = new int[cap];
-    }
-
-    void push(int val){
-        if (size == cap) {
-            System.out.println("Queue is full");
-            return;
-        }else if (top == -1) {
-            top = end = 0;
-        }else{
-            end = (end + 1 ) % 10;
-        }
-
-        que[end] = val;
-    }
-
-    void pop(){
-        if (size == 0) {
-            System.out.println("queue is empty");
-            return;
-        }
-
-        top++;
-    }
-    
-}
-
-
-class stkLL {
-
-    LinkedLst top;
-    int size = 0;
-
-    void push(int val){
-       LinkedLst newNode = new LinkedLst(val);
-       top.next = newNode;
-       top = newNode;
-       size++;
-    }
-
-    void pop(){
-        if (size == 0) {
-            System.out.println("stk is empty");
-            return;
-        }
-
-        this.top = this.top.next;
-        size--;
-    }
-    
-}
-
-class queueLL {
-    LinkedLst top, end;
-    int size = 0;
-    
-    void push(int val){
-        LinkedLst newNode = new LinkedLst(val);
-
-        if (top == null) {
-            top = end = newNode;
-        }else{
-            end.next = newNode;
-            end = newNode;
+        Node(int data){
+            this.data = data;
+            this.left = this.right = null;
         }
     }
-}
+
 public class Solution {
+    Node root;
+
+    void insert(int val){
+        root = insertrec(val, root);
+    }
+
+    Node insertrec(int val, Node root){
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (val < root.data) {
+            root.left = insertrec(val, root.left);
+        }else if (val > root.data) {
+            root.right = insertrec(val, root.right);
+        }
+
+        return root;
+    }
+
+    void PostOrderTraversal(Node root){
+        if (root != null) {
+            PostOrderTraversal(root.left);
+            PostOrderTraversal(root.right);
+            System.out.println("-->" + root.data);
+        }
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("sddgf");
+        System.out.println("Working");
     }
 }
