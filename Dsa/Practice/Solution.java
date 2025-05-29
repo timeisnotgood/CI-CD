fabd4b36ea1dcfb34527427c01313e1c9ba1fd53
bhoopathi <bhoopathicl@gmail.com>
2025-05-29 07:47:33 +0530

    graph revice

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,104 +1,50 @@
 package Practice;
+import java.util.ArrayList;
 
 public class Solution {
 
-    int[] heap;
-    int size;
-    int cap;
+    ArrayList<ArrayList<Integer>> list;
+    int v;
 
-    Solution(int cap){
-        this.cap = cap;
-        this.size = 0;
-        this.heap = new int[cap];
-    }
-
-    int getParentIndex(int i){return (i - 1) / 2;}
-    int getLeftIndex(int i){return (i + 1) * 2;}
-    int getRightIndex(int i){return (i + 2) * 2;}
-
-    int parent(int i){return heap[getParentIndex(i)];}
-    int left(int i){return heap[getLeftIndex(i)];}
-    int right(int i){return heap[getRightIndex(i)];}
-
-    boolean hasParent(int i){return getParentIndex(i) >= 0;}
-    boolean hasLeft(int i){return getLeftIndex(i) < size;}
-    boolean hasRight(int i){return getRightIndex(i) < size;}
-
-    void swap(int i, int j){
-        int temp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = temp;
-    }
-
-    void heapfyUp(){
-        int index = size - 1;
-        while (hasParent(index) && parent(index) > heap[index]) {
-            swap(getParentIndex(index), index);
-            index = getParentIndex(index);
+    Solution(int v){
+        this.v = v;
+        this.list = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            list.add(new ArrayList<>());
         }
     }
 
-    void heapDown(){
-        int index = 0;
-        while (hasLeft(index)) {
-            int smallest = getLeftIndex(index);
-            if (hasRight(index) && right(index) < left(index)) {
-                smallest = getRightIndex(index);
+    void addEdges(int v, int e){
+        list.get(v).add(e);
+        list.get(e).add(v);
+    }
+
+    void removeEdges(int v, int e){
+        list.get(v).add(0);
+        list.get(e).add(0);
+    }
+
+    void printGraph(){
+
+        for (int i = 0; i < v; i++) {
+            System.out.print("Vertex " + i + ":");
+            for(int data: this.list.get(i)){
+                System.out.println(" -- " + data );
             }
-            if (heap[index] > heap[smallest]) {break;}
-            else{swap(index, smallest);}
-            index = smallest;
+            System.out.println();
         }
-    }
-
-    void insert(int val){
-        if (size == cap) {
-            System.out.println("heap is Bull");
-            return;
-        }
-
-        heap[size] = val;
-        size++;
-        heapfyUp();
-    }
-
-    void extractMin(){
-        if (size == 0) {
-            System.out.println(" Heap is Empty !!");
-            return;
-        }
-
-        int deleted = heap[0];
-        heap[0] = heap[size - 1];
-        size--;
-        heapDown();
-    }
-
-    void findKsmallestElement(int[] arr, int k){
-        Solution heap = new Solution(k);
-
-       for(int num : arr){
-            heap.insert(num);
-            if (heap.size > k) {
-                extractMin();
-            }
-       }
-        for(int i = 0; i < heap.size; i++){
-            System.out.println("- >  " + heap.heap[i]);
-        }
-    }
-
-    // Print the heap
-    public void printHeap() {
-        for (int i = 0; i < size; i++) {
-            System.out.print(heap[i] + " ");
-        }
-        System.out.println();
     }
 
     public static void main(String[] args) {
-        Solution obj = new Solution(10);
-        int[] arr = {10, 3, 5, 7, 6, 2, 8, 1, 9};
-        obj.findKsmallestElement(arr, 3);
+        Solution graph = new Solution(6);
+
+        graph.addEdges(0, 1);
+        graph.addEdges(1, 2);
+        graph.addEdges(1, 3);
+        graph.addEdges(2, 4);
+        graph.addEdges(3, 5);
+
+        graph.printGraph();
     }
+
 }
