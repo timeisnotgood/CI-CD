08b16fb7da3660ce3e66a78ec9776a7a5b1e4a6e
bhoopathi <bhoopathicl@gmail.com>
2025-06-05 19:16:11 +0530

    sorting searching prac

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,130 +1,109 @@
 package Practice;
 
+import java.util.ArrayList;
+
 public class Solution {
 
-    int[] heap;
-    int cap, size;
-
-    Solution(int cap){
-        this.cap = cap;
-        this.size = 0;
-        this.heap = new int[cap];
-    }
-
-    int getParentIndex(int i) {return (i - 1) /2;}
-    int getLeftIndex(int i) {return 2 * i + 1;}
-    int getRightIndex(int i) {return 2 * i + 2;}
-
-    boolean hasParent(int i){return getParentIndex(i) >= 0;}
-    boolean hasLeft(int i){return getLeftIndex(i) < size;}
-    boolean hasRight(int i){return getRightIndex(i) < size;}
-
-    int Parent(int i){return heap[getParentIndex(i)];}
-    int Left(int i){return heap[getLeftIndex(i)];}
-    int Right(int i){return heap[getRightIndex(i)];}
-
-    void swap(int i, int j){
-        int temp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = temp;
-    }
-
-    void heapfyUp(){
-        int index = size - 1;
-
-        while (hasParent(index) && Parent(index) > heap[index]) {
-            swap(getParentIndex(index), index);
-            index =getParentIndex(index);
+    void mergeSort(int[] arr, int low, int high){
+        if(low < high){
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid+1, high);
+            merge(arr, low, mid, high);
         }
     }
 
-    void heapfyDown(){
-        int index= 0;
+    void merge(int[] arr, int low, int mid, int high){
+        ArrayList<Integer> list = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
 
-        while (hasLeft(index)) {
-            int smallest = getLeftIndex(index);
-
-            if (hasRight(index) && Right(index) < Left(index)) {
-                smallest = getRightIndex(index);
-            }
-
-            if (heap[index] < heap[smallest]) {
-                break;
+        while (left <= mid && right <= high) {
+            if(arr[left] <= arr[right]){
+                list.add(arr[left]);
+                left++;
             }else{
-                swap(index, smallest);
-            }
-            index = smallest;
-        }
-    }
-
-
-    void insert(int val){
-        if (size == cap) {
-            System.out.println("Heap is Bull");
-            return;
-        }
-
-        heap[size] = val;
-        size++;
-        heapfyUp();
-    }
-
-    void findFsmallest(int[] arr, int k){
-        Solution heap = new Solution(10);
-
-        for (int num : arr) {
-            heap.insert(num);
-            if (heap.size > k) {
-                // heap.extractMin();
+                list.add(arr[right]);
+                right++;
             }
         }
 
-        for (int i = 0; i < heap.size; i++) {
-            System.out.println("Heap --> " + heap.heap[i]);
+        while (left <= mid) {
+            list.add(arr[left]);
+                left++;
+        }
+
+        while (right <= high) {
+            list.add(arr[right]);
+                right++;
+        }
+
+        for(int i = low; i <= high; i++){
+            arr[i] = list.get(i - low);
         }
     }
 
-    void leftRotate(int arr[], int k) {
-        for (int j = 0; j <= k; j++) {
-           int num = arr[0];
-           for (int i = 0; i < arr.length; i++) {
-               if(i == arr.length - 1){
-                   arr[i] = num;
-
-                   break;
-               }
-               arr[i] = arr[i + 1];
-           }
-       }
-    }
-
-
-    void leftRotates(int arr[], int k) {
-        int n = arr.length;
-        k %= n;
-        if (k == 0) return;
-        rev(arr, 0, k - 1);
-        rev(arr, k, n - 1);
-        rev(arr, 0, n - 1);
-    }
-
-    private void rev(int a[], int low, int high) {
-        while (low < high) {
-            swap(a, low++, high--);
+    void quickSort(int[] arr, int low, int high){
+        if(low < high){
+            int peviotIndex = partetion(arr, low, high);
+            quickSort(arr, low, peviotIndex - 1);
+            quickSort(arr, peviotIndex + 1, high);
         }
     }
 
-    private void swap(int a[], int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+    int partetion(int[] arr, int low, int high){
+        int peviot = arr[high];
+        int i = low - 1;
+
+        for(int j = low; j < high;j++){
+            if (arr[j] <= peviot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+    int binarySearch(int[] arr, int k){
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == k) {
+                return mid;
+            }
+
+            if(arr[mid] < k){
+                left = mid - 1;
+            }
+
+            if(arr[mid] > k){
+                right = mid + 1;
+            }
+        }
+
+        return - 1;
     }
 
     public static void main(String[] args) {
-        Solution obj = new Solution(10);
+        Solution obj = new Solution();
         int[] arr = {1,2,3,4,5,6};
-        obj.leftRotate(arr, 2);
+        System.out.println(
+
+            obj.binarySearch(arr, 2)
+        );
+        
+        // for(int num : arr){
+        //     System.out.print(num + " ");
+        // }
     }
-
-
 }
