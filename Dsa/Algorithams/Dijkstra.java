7dcf6ade41480f0f3a3dbdd58582419b5b047093
bhoopathi <bhoopathicl@gmail.com>
2025-06-19 07:34:06 +0530

    dijkstra prac

--- Dsa/Algorithams/Dijkstra.java
+++ Dsa/Algorithams/Dijkstra.java
@@ -1,6 +1,7 @@
 package Algorithams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 class Pair {
@@ -31,16 +32,47 @@ public class Dijkstra {
             int dis =  pq.peek().distance; 
             int node =  pq.peek().node; 
             pq.remove(); 
-            for(int i = 0; i<adj.get(node).size();i++) { 
+
+        System.out.println("------->"+adj.get(node).size());
+
+            for(int i = 0; i< adj.get(node).size();i++) { 
+
                 int edgeWeight = adj.get(node).get(i).get(1); 
                 int adjNode =  adj.get(node).get(i).get(0); 
+
                 if(dis + edgeWeight < dist[adjNode]) { 
                     dist[adjNode] = dis + edgeWeight; 
                     pq.add(new Pair(dist[adjNode], adjNode)); 
                 } 
+
             } 
         } 
+        
         return dist; 
 
     }
+
+
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Edges
+        adj.get(0).add(new ArrayList<>(Arrays.asList(1, 2)));
+        adj.get(0).add(new ArrayList<>(Arrays.asList(2, 4)));
+        adj.get(1).add(new ArrayList<>(Arrays.asList(2, 1)));
+        adj.get(1).add(new ArrayList<>(Arrays.asList(3, 7)));
+        adj.get(2).add(new ArrayList<>(Arrays.asList(4, 3)));
+        adj.get(3).add(new ArrayList<>(Arrays.asList(4, 1)));
+
+        int[] result = dijkstra(V, adj, 0);
+
+        // System.out.println("Shortest distances from node 0:");
+        // System.out.println(Arrays.toString(result));
+    }
+
 }
