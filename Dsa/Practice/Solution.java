03c7827056654327da4b47e77572d429bfafaa21
bhoopathi <bhoopathicl@gmail.com>
2025-06-27 11:00:23 +0530

    prefix Sum practice

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -20,16 +20,39 @@ public class Solution {
        return temp;
     }
 
+    public int[] runningSum(int[] nums) {
+        int[] temp = new int[nums.length];
+        temp[0] = nums[0];
+        for(int i = 1; i < nums.length;i++){
+            temp[i] = temp[i-1] + nums[i];
+        }
+        return temp;
+    }
+
+    public int pivotIndex(int[] nums) {
+       int totla = 0;
+       for(int num : nums){ totla += num;}
+
+       int leftSum = 0;
+       for(int i = 0;i < nums.length; i++){
+            if(totla == leftSum - totla - nums[i]){
+                return i;
+            }
+            leftSum += nums[i];
+       }
+
+       return -1;
+        
+    }
+
     public static void main(String[] args) {
-        System.out.println("Main -->");
+        // System.out.println("Main -->");
 
         Solution obj = new Solution();
-        int[] arr = {1, 9, 9};
-        // obj.plusOne(arr);
-        int[] added = obj.plusOne(arr);
+        int[] arr = {1,7,3,6,5,6};
 
-        for(int num : added){
-            System.out.println("--> "+ num);
-        }
+        int piv = obj.pivotIndex(arr);
+
+        System.out.println("---->"+ piv);
     }
 }
