b3ad4d8994bc90ccde770ee0cd9432669e09ca3f
bhoopathi <bhoopathicl@gmail.com>
2025-06-29 21:20:11 +0530

    mergin practice

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -20,14 +20,71 @@ public class Solution {
        return temp;
     }
 
+    // public void merge(int[] nums1, int m, int[] nums2, int n) {
+    //     int[] temp = new int[m + n];
+    //     int i = 0, j = 0, a = 0;
+
+    //     while (i < m && j < n) {
+    //         System.out.println(i + " " + j);
+    //         if (nums1[i] < nums2[j]) {
+    //             temp[a] = nums1[i];
+    //             i++;
+    //             a++;
+    //         }else{
+    //             temp[a] = nums2[j];
+    //             j++;
+    //             a++;
+    //         }
+    //     }
+
+    //     while (i < m) {
+    //         temp[a] = nums1[i];
+    //             i++;
+    //             a++;
+    //     }
+
+    //     while (j < n) {
+    //         temp[a] = nums2[j];
+    //             j++;
+    //             a++;
+    //     }
+
+    //     for(int k = 0 ;k < nums1.length;k++){
+    //         nums1[k] = temp[k];
+    //     }
+    // }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+       int i = m - 1;
+       int j = n - 1;
+       int k = m + n -1;
+
+       while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+       }
+
+       while (j > 0) {
+         nums1[k--] = nums2[j--];
+       }
+    }
+
     public static void main(String[] args) {
         // System.out.println("Main -->");
 
         Solution obj = new Solution();
-        int[] arr = {1,7,3,6,5,6};
+        int[] num1 = {1,2,3,0,0,0};
+        int[] num2 = {2,5,6};
 
-        int piv = obj.pivotIndex(arr);
+        obj.merge(num1, 3, num2, num2.length);
 
-        System.out.println("---->"+ piv);
+        for(int num : num1){
+            System.out.println("-->" + num);
+        }
+
+        // System.out.println("---->"+ piv);
     }
 }
