888e034b672f1d2ce0425191386a1c65e3019b30
bhoopathi <bhoopathicl@gmail.com>
2025-07-13 16:00:48 +0530

    Sorting pratice

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -40,13 +40,47 @@ public class Solution {
         arr[j] = temp;
     }
 
+    void SelectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) min = j;
+            }
+            swap(arr, i, min);
+        }
+    }
+
+    void bubbleSort(int[] arr){
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for(int j = 0; j <= i - 1; j ++){
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+            
+        }
+    }
+
+    void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                swap(arr, j - 1, j);
+                j--;
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("hello");
 
         int[] arr = {2,3,7,5,4,1,6};
 
         Solution obj = new Solution();
-        obj.quickSort(arr, 0, arr.length - 1);
+        obj.insertionSort(arr);
 
         for(int num : arr){
             System.out.print(num + " ");
