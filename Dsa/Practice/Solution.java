1bdb8d472855f34e86a494bd085cac85b8b33db6
bhoopathi <bhoopathi.sundarasamy@lystloc.com>
2025-09-13 15:10:01 +0530

    sorting revice

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -9,9 +9,9 @@ public class Solution {
     }
 
     void selectSort(int[] arr, int n){
-        for (int i = 0; i <= n-2 ; i++) {
+        for(int i = 0;i <= n - 2;i++){
             int min = i;
-            for(int j = i; j <= n - 1; j++){
+            for(int j = i;j <= n -1;j++){
                 if(arr[j] < arr[min]) min = j;
             }
             swap(arr, i, min);
@@ -19,29 +19,28 @@ public class Solution {
     }
 
     void bubbleSort(int[] arr, int n){
-        for (int i = n - 1; i >= 1; i--) {
-            for(int j = 0; j <= i - 1;j++){
-                if(arr[j] > arr[j+1]) swap(arr, j, j + 1);
+        for(int i = n - 1;i >= 1;i--){
+            for(int j = 0;j <= i - 1;j++){
+                if(arr[j] > arr[j + 1]) swap(arr, j, j+1);
             }
         }
     }
 
     void insertionSort(int[] arr, int n){
-        for(int i = 0;i <= n - 1;i++){
+        for(int i = 0; i <= n - 1;i++){
             int j = i;
-            while (j > 0 && arr[j - 1] > arr[j]) {
-                swap(arr, j-1, j);
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                swap(arr, j, j - 1);
                 j--;
             }
         }
     }
 
-
     public static void main(String[] args) {
         Solution obj = new Solution();
 
-        int[] arr = {6,2,8,99,1};
-        obj.selectSort(arr, arr.length);
+        int[] arr = {6,2,8,99,3,1};
+        obj.insertionSort(arr, arr.length);
         
         for(int i : arr){
             System.out.print(i + " ");
