8a10fa4ae0fb07e06586d5eed4e3eab77975d85e
bhoopathi <bhoopathi.sundarasamy@lystloc.com>
2025-09-17 22:31:45 +0530

    merge sort practiced

--- Dsa/Practice/Solution.java
+++ Dsa/Practice/Solution.java
@@ -1,5 +1,7 @@
 package Practice;
 
+import java.util.ArrayList;
+
 public class Solution {
 
     void swap(int[] arr, int i, int j){
@@ -8,39 +10,88 @@ public class Solution {
         arr[j] = temp;
     }
 
-    void selectSort(int[] arr, int n){
-        for(int i = 0;i <= n - 2;i++){
+    void mergeSort(int[] arr, int low, int high){
+        if(low < high){
+            int mid = (low+high)/2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid+1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    void merge(int[] arr, int low, int mid, int high){
+        ArrayList<Integer> list = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] < arr[right]) {
+                list.add(arr[left]);
+                left++;
+            }else{
+                list.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            list.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            list.add(arr[right]);
+            right++;
+        }
+
+        for(int i = low;i <= high;i++){
+            arr[i] = list.get(i - low);
+        }
+    }
+
+    void ses(int[] arr){
+        int n = arr.length;
+
+        for (int i = 0; i <= n - 2; i++) {
             int min = i;
-            for(int j = i;j <= n -1;j++){
+            for (int j = i; j <= n - 1; j++) {
                 if(arr[j] < arr[min]) min = j;
             }
             swap(arr, i, min);
         }
     }
 
-    void bubbleSort(int[] arr, int n){
-        for(int i = n - 1;i >= 1;i--){
-            for(int j = 0;j <= i - 1;j++){
-                if(arr[j] > arr[j + 1]) swap(arr, j, j+1);
+
+    void bubble_s(int[] arr){
+        int n = arr.length;
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if(arr[j] > arr[j + 1])
+                swap(arr, j + 1, j);
             }
         }
     }
 
-    void insertionSort(int[] arr, int n){
-        for(int i = 0; i <= n - 1;i++){
+    void insertion_sort(int[] arr){
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
             int j = i;
-            while (j > 0 && arr[j] < arr[j - 1]) {
-                swap(arr, j, j - 1);
+
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                swap(arr, j - 1, j);
                 j--;
             }
         }
     }
 
+
     public static void main(String[] args) {
         Solution obj = new Solution();
 
         int[] arr = {6,2,8,99,3,1};
-        obj.insertionSort(arr, arr.length);
+        obj.insertion_sort(arr);
         
         for(int i : arr){
             System.out.print(i + " ");
